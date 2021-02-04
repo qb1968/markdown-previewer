226b986edad4651634133c7b0a24681d348a7b4d
qb1968 <qb1968@aol.com>
2021-02-03 19:23:31 -0500

    Third

---
+++ src/directives/decode.js
@@ -0,0 +1,21 @@
+const rules = [
+    [/^(?:######)\s*(.+?)[ \t]*$/gm, '<h6>$1</h6>'],
+    [/^(?:#####)\s*(.+?)[ \t]*$/gm, '<h5>$1</h5>'],
+    [/^(?:####)\s*(.+?)[ \t]*$/gm, '<h4>$1</h4>'],
+    [/^(?:###)\s*(.+?)[ \t]*$/gm, '<h3>$1</h3>'],
+    [/^(?:##)\s*(.+?)[ \t]*$/gm, '<h2>$1</h2>'],
+    [/^(?:#)\s*(.+?)[ \t]*$/gm, '<h1>$1</h1>'],
+    [/^(?:-)\s*(.+?)[ \t]*$/gm, '<ul><li>$1</li></ul>'],
+    [/^(?:>)\s*(.+?)[ \t]*$/gm, ' <blockquote>$1</blockquote>']
+  ]
+  export default {
+    bind(el) {
+      let content = el.textContent
+      rules.forEach(([rule, template]) => {
+  
+        content = content.replace(rule, template);
+        console.log(content)
+      })
+      el.innerHTML = content
+    }
+  }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+// import decode from '@/directives/decode'
+// Vue.directive('decode', decode)
 
-createApp(App).use(router).mount('#app')
+import VueDirectiveMarkdown from 'vue-directive-markdown'
+Vue.directive('decode', VueDirectiveMarkdown)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
